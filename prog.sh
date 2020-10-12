43b13127336e04cb538f1cb7b35fed2e53fe1079
baraka <adarsh.ninganur@gmail.com>
2020-10-12 21:48:01 +0530

    TOP 10 files

--- prog.sh
+++ prog.sh
@@ -1,15 +1,2 @@
-#!/bin/bash
-FILE="./data.vcf"
-
-while IFS=, read -r FName LName DOB Tel
-do
-    echo "BEGIN:VCARD"  >> $FILE
-    echo "VERSION:3.0" >> $FILE
-    echo "N:$LName;$FName;;;" >> $FILE
-    echo "FN:$FName $LName" >> $FILE
-    echo "TEL;TYPE=HOME:$Tel" >> $FILE
-    echo "BDAY:$DOB" >> $FILE
-    echo "END:VCARD" >> $FILE
-done < data.csv
-
+find . -type f -mtime -20 -printf "%s" -ls | sort -rn | head -n 10
 
